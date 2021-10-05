81271f6d476ca24d1e60341c94e5e3211053352c
Razvan Rotari <razvanrotari@posteo.net>
2021-10-05 19:36:25 +0300

    Initial commit for parterm

--- README.md
+++ README.md
@@ -1,48 +1,27 @@
-# term_mux_rs (working title, I don't like it, see [#1](https://github.com/mdevlamynck/term_mux_rs/issues/1))
+# Parterm
 
-[![Build Status](https://travis-ci.org/mdevlamynck/term_mux_rs.svg?branch=master)](https://travis-ci.org/mdevlamynck/term_mux_rs)
+Remote control for your terminal. Allows you to execute arbitary commands in a different terminal. One usecase is to trigger the compilation in a different terminal from your editor.
 
-Terminal multiplexer in rust
-
-This project only supports GNU/Linux at the moment. Redox, macOS and Windows support may happen in the future (in that order of priority).
+This project only supports GNU/Linux at the moment. 
 
 # Installation
 
-You can use `cargo install` to install this project. It will compile the binary `term_mux` and install it in the `~/.cargo/bin` folder. Make sure this folder is in your path if you want to be able to run it directly.
+You can use `cargo install` to install this project. It will compile the binary `parterm` and install it in the `~/.cargo/bin` folder. Make sure this folder is in your path if you want to be able to run it directly.
 
-```sh
-cargo install --git https://github.com/mdevlamynck/term_mux_rs
 
-# or you can specify a branch with --branch
-cargo install --git https://github.com/mdevlamynck/term_mux_rs --branch dev
+This project is not ready to be used yet. Once the project is ready, it will be published on crates.io and you will be able to install the latest release with a simple `cargo install parterm`.
+
+# Usage
+
+Start the server
+```
+parterm server
 ```
 
-If you want the last stable version (i.e. release), use the master branch.
-If you want the last development version, use the dev branch.
-
-This project is not ready to be used yet. Once the project is ready, it will be published on crates.io and you will be able to install the latest release with a simple `cargo install term_mux_rs`.
-
-# Hacking
-
-As any rust project, use `cargo` to build, run the project, run the tests or build the docs.
-
-```sh
-cargo build      # compile
-cargo run        # launch term_mux
-cargo test       # run tests
-cargo doc        # build the docs
-cargo doc --open # build the docs and open them in your browser
+In another terminal run
+```
+parterm client -- ls
 ```
 
-As a bonus, if you want to see the full documentation, including the docs of private elements, use :
+This will run "ls" in the first terminal.
 
-```sh
-cargo rustdoc -- --no-defaults --passes collapse-docs --passes unindent-comments --passes strip-priv-imports
-
-# or the version with --open
-cargo rustdoc --open -- --no-defaults --passes collapse-docs --passes unindent-comments --passes strip-priv-imports
-```
-
-This doc also includes the documentation of libraries term_mux_rs depends on so it can be really usefull when working on the project.
-
-As for the documentation of rust itself, if you're using `rustup` you can use `rustup doc`.
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,146 @@
-extern crate term_mux;
-extern crate termion;
 extern crate chan_signal;
+extern crate clap;
+extern crate termion;
+extern crate parterm;
 
-use std::io::{Read, Write, Result};
+use chan_signal::{notify, Signal};
+use clap::{App, Arg, SubCommand};
 use std::fs::File;
+use std::io::{Read, Result, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 use std::time::Duration;
+use parterm::get_shell;
+use parterm::pty::Pty;
+use parterm::tui::{get_terminal_size, Size};
 use termion::get_tty;
 use termion::raw::IntoRawMode;
-use chan_signal::{notify, Signal};
-use term_mux::pty::Pty;
-use term_mux::tui::{get_terminal_size, Size};
-use term_mux::get_shell;
 
-fn main () {
-    let signal         = notify(&[Signal::WINCH]);
+static ADDRESS: &str = "127.0.0.1:10000";
+
+fn client(value: String) {
+    if let Ok(mut stream)  = TcpStream::connect(ADDRESS) {
+        stream.write(value.as_bytes());
+        stream.shutdown(std::net::Shutdown::Both);
+    } else {
+        println!("Unable to connect to server {}", ADDRESS);
+    }
+}
+
+fn main() {
+    let matches = App::new("parterm")
+        .version("0.1")
+        .author("Razvan Rotari <razvanrotari@posteo.net>")
+        .about("Remote control for your terminal")
+        .setting(clap::AppSettings::TrailingVarArg)
+        .setting(clap::AppSettings::AllowLeadingHyphen)
+        .subcommand(SubCommand::with_name("client")
+                    .about("")
+
+                    .arg(Arg::with_name("cmd")
+                         .help("<command>")
+                         .required(true)
+                         .takes_value(true)
+                         .multiple(true)
+                         .last(true)
+                        ))
+        .subcommand(SubCommand::with_name("server")
+                    .about(""))
+        .get_matches();
+
+    if let Some(client_sub) = matches.subcommand_matches("client") {
+        match client_sub.value_of("cmd") {
+            Some(val) => client(val.to_owned() + "\n"),
+            _ => println!("{}", matches.usage()),
+        }
+        return;
+    }
+    let signal = notify(&[Signal::WINCH]);
 
     let mut tty_output = get_tty().unwrap().into_raw_mode().unwrap();
-    let mut tty_input  = tty_output.try_clone().unwrap();
+    let mut tty_input = tty_output.try_clone().unwrap();
 
-    let pty_resize     = Pty::spawn(&get_shell(), &get_terminal_size().unwrap()).unwrap();
+    let pty_resize = Pty::spawn(&get_shell(), &get_terminal_size().unwrap()).unwrap();
     let mut pty_output = pty_resize.try_clone().unwrap();
-    let mut pty_input  = pty_output.try_clone().unwrap();
+    let mut pty_input = pty_output.try_clone().unwrap();
 
-    let handle = thread::spawn(move || {
-        loop {
-            match pipe(&mut pty_input, &mut tty_output) {
-                Err(_) => return,
-                _      => (),
-            }
+    let (cmd_sender, cmd_receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
+    let (val_sender, val_receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
+
+    let handle = thread::spawn(move || loop {
+        match pipe(&mut pty_input, &mut tty_output) {
+            Err(_) => return,
+            _ => (),
         }
     });
 
-    thread::spawn(move || {
-        loop {
-            match pipe(&mut tty_input, &mut pty_output) {
-                Err(_) => return,
-                _      => (),
-            }
-        }
+    thread::spawn(move || loop {
+        let mut packet = [0; 4096];
+
+        let count = tty_input.read(&mut packet).unwrap();
+        let (sub_slice,_) = packet.split_at(count);
+        val_sender.send(sub_slice.to_vec());
     });
 
-    thread::spawn(move || {
-        loop {
-            signal.recv().unwrap();
-            pty_resize.resize(&get_terminal_size().unwrap());
-        }
+    thread::spawn(move || handle_slave_output(cmd_receiver, val_receiver, pty_output));
+
+    thread::spawn(move || loop {
+        signal.recv().unwrap();
+        pty_resize.resize(&get_terminal_size().unwrap());
     });
+
+    thread::spawn(move || read_comands(cmd_sender));
 
     handle.join();
+}
+
+fn read_comands(cmd_sender: Sender<Vec<u8>>) {
+    let listener = TcpListener::bind(ADDRESS).unwrap();
+
+    for streamRes in listener.incoming() {
+        let mut stream = streamRes.unwrap();
+        stream.set_read_timeout(Some(Duration::from_millis(1)));
+        loop {
+            let mut data = [0; 256];
+            match stream.read(&mut data) {
+                Ok(bytes) => {
+                    if bytes == 0 {
+                        break;
+                    }
+                    cmd_sender.send(Vec::from(data));
+                }
+                Err(err) => {
+                    println!("{}", err);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn handle_slave_output(
+    cmd_receiver: Receiver<Vec<u8>>,
+    val_receiver: Receiver<Vec<u8>>,
+    mut pty_output: File,
+    ) {
+    loop {
+        match cmd_receiver.try_recv() {
+            Ok(val) => {
+                pty_output.write_all(val.as_slice());
+                pty_output.flush();
+            }
+            _ => (),
+        }
+
+        match val_receiver.try_recv() {
+            Ok(val) => {
+                pty_output.write_all(val.as_slice());
+                pty_output.flush();
+            }
+            _ => (),
+        }
+    }
 }
 
 /// Sends the content of input into output
